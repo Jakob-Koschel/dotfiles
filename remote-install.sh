ef97b116779f3d4591bdc9223b67d5da657c9d32
Jakob Koschel <jakobkoschel@aim.com>
2021-12-01 00:38:58 +0100

    add remote install support

---
+++ remote-install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# can be ran with:
+# curl -o- -L https://raw.githubusercontent.com/Jakob-Koschel/dotfiles/zero/remote-install.sh | bash
+
+set -e
+
+git clone https://github.com/Jakob-Koschel/dotfiles.git
+cd dotfiles
+./setup.sh
--- setup.sh
+++ setup.sh
@@ -11,11 +11,24 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 	OS_NAME="linux"
 fi
 
+# check if remote is using HTTPS, if so offer replacment with SSH
+GIT_REMOTE_URL=$(git remote get-url origin)
+if [[ $GIT_REMOTE_URL == https* ]]; then
+  read -p "Repository is cloned via HTTPS. Should this be replaced with SSH? [yN]: " -n 1 -r; echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    REPO=$(echo $GIT_REMOTE_URL | sed -n 's@^.*github.com/*/\([^)]*\).*$@\1@p')
+    USERNAME=${REPO%/*}
+    REPOSITORY=${REPO#*/}
+    git remote set-url origin "git@github.com:$USERNAME/$REPOSITORY"
+  fi
+fi
+
 git submodule init && git submodule update
 
 echo "Run $DOTFILES_ROOT/bashdot/bashdot install default $OS_NAME..."
 PROFILES="default $OS_NAME"
 
+# check if repo is still locked and should be unlocked
 if [ -s $DOTFILES_ROOT/git-crypt-status ]; then
   read -p "Dotfiles are still encrypted, do you want to decrypt? [yN]: " -n 1 -r; echo
   if [[ $REPLY =~ ^[Yy]$ ]]; then
