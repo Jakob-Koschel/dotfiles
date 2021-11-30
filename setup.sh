3c616196d5696710dfbee5b32e27910bd15f4245
Jakob Koschel <jakobkoschel@aim.com>
2021-11-30 23:48:24 +0100

    update

--- setup.sh
+++ setup.sh
@@ -14,4 +14,16 @@ fi
 git submodule init && git submodule update
 
 echo "Run $DOTFILES_ROOT/bashdot/bashdot install default $OS_NAME..."
-$DOTFILES_ROOT/bashdot install default $OS_NAME
+PROFILES="default $OS_NAME"
+
+if [ -s $DOTFILES_ROOT/git-crypt-status ]; then
+  read -p "Dotfiles are still encrypted, do you want to decrypt? [yN]: " -n 1 -r; echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    git-crypt unlock
+    PROFILES="$PROFILES $OS_NAME-crypt"
+  fi
+else
+  PROFILES="$PROFILES $OS_NAME-crypt"
+fi
+
+$DOTFILES_ROOT/bashdot install $PROFILES
