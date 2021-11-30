4530f9b8b0e670124c96a80f4ca4b993fe19c82a
Jakob Koschel <jakobkoschel@aim.com>
2021-12-01 00:52:04 +0100

    fix

--- remote-install.sh
+++ remote-install.sh
@@ -3,9 +3,9 @@
 # can be ran with:
 # curl -o- -L https://raw.githubusercontent.com/Jakob-Koschel/dotfiles/zero/remote-install.sh | bash
 
-git clone https://github.com/Jakob-Koschel/dotfiles.git
+set -e
 
-echo "TESTING"
+git clone https://github.com/Jakob-Koschel/dotfiles.git
 cd dotfiles
 git checkout zero
 # ./setup.sh
