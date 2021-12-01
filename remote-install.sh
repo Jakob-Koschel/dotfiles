ca3e8d2d45967f23a9bb593e595c22202bf4d155
Jakob Koschel <jakobkoschel@aim.com>
2021-12-01 01:21:13 +0100

    update hint on how to run remote-install

--- remote-install.sh
+++ remote-install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # can be ran with:
-# curl -o- -L https://raw.githubusercontent.com/Jakob-Koschel/dotfiles/zero/remote-install.sh | bash
+# bash <(curl -L -s https://raw.githubusercontent.com/Jakob-Koschel/dotfiles/zero/remote-install.sh)
 
 set -e
 
 git clone https://github.com/Jakob-Koschel/dotfiles.git
 cd dotfiles
 git checkout zero
-bash setup.sh
+./setup.sh
