c220d1caa99e5f85cba160e0944ba4fb4bf995de
Jakob Koschel <jakobkoschel@aim.com>
2021-12-03 12:46:26 +0100

    change to master branch

--- remote-install.sh
+++ remote-install.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
 # can be ran with:
-# bash <(curl -L -s https://raw.githubusercontent.com/Jakob-Koschel/dotfiles/zero/remote-install.sh)
+# bash <(curl -L -s https://raw.githubusercontent.com/Jakob-Koschel/dotfiles/master/remote-install.sh)
 
 set -e
 
 git clone https://github.com/Jakob-Koschel/dotfiles.git
 cd dotfiles
-git checkout zero
 ./setup.sh
