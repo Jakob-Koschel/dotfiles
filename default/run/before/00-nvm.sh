99f2c2984339681ee22ddf1b39a7f900dfe75318
Jakob Koschel <jakobkoschel@gmail.com>
2022-04-01 17:38:49 +0200

    00-nvm.sh: fix up

--- default/run/before/00-nvm.sh
+++ default/run/before/00-nvm.sh
@@ -3,8 +3,8 @@
 set -e
 
 if [ -n $NVM_DIR ]; then
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 fi
 
 # Check for nvm,
@@ -12,6 +12,13 @@ fi
 if ! command -v nvm >/dev/null; then
   echo "Installing nvm..."
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+fi
+
+# try loading it again
+NVM_DIR=$HOME/.nvm
+if [ -n $NVM_DIR ]; then
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 fi
 
 nvm install --lts --default
