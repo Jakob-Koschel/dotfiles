5e4b546f9bc15b7e4e599858bab960a8cd5abb63
Jakob Koschel <jakobkoschel@gmail.com>
2022-06-02 01:09:35 +0200

    apt: install neovim, fzf, clangd, bat ripgrep

--- ubuntu/run/before/00-apt.sh
+++ ubuntu/run/before/00-apt.sh
@@ -7,14 +7,22 @@ sudo apt-get install -y \
   wget \
   zsh \
   vim \
+  neovim \
+  fzf \
   git \
   curl \
   tmux \
   python3 \
   python3-dev \
   clang \
+  clangd \
   llvm \
   cmake \
   time \
   build-essential \
   sudo
+
+# https://github.com/BurntSushi/ripgrep/issues/1777#issuecomment-866964695
+sudo apt install -o Dpkg::Options::="--force-overwrite" bat ripgrep
+
+ln -s /usr/bin/batcat ~/.local/bin/bat
