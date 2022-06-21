025219d54130e8defba14322f110d48b7441beb4
Jakob Koschel <jakobkoschel@gmail.com>
2022-06-21 14:51:07 +0200

    apt.sh: create ~/.local/bin if not exists

--- ubuntu/run/before/00-apt.sh
+++ ubuntu/run/before/00-apt.sh
@@ -29,4 +29,5 @@ fi
 # https://github.com/BurntSushi/ripgrep/issues/1777#issuecomment-866964695
 sudo apt install -o Dpkg::Options::="--force-overwrite" bat ripgrep
 
+mkdir -p ~/.local/bin
 ln -sfn /usr/bin/batcat ~/.local/bin/bat
