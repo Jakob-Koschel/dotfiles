0cf0bad165b8b6ae89eb92aca04382a78b2380ac
Jakob Koschel <jkl820.git@gmail.com>
2022-08-01 22:46:47 +0200

    separate ubuntu and linux profiles

--- setup.sh
+++ setup.sh
@@ -5,18 +5,6 @@ set -e
 cd "$(dirname "$0")"
 DOTFILES_ROOT=$(pwd -P)
 
-if [ "$(uname)" == "Darwin" ]; then
-	OS_NAME="macos"
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-	OS_NAME="ubuntu"
-
-  # check if stow is installed
-  if ! command -v stow &> /dev/null
-  then
-    sudo apt update && sudo apt install -y stow
-  fi
-fi
-
 # check if remote is using HTTPS, if so offer replacment with SSH
 GIT_REMOTE_URL=$(git remote get-url origin)
 if [[ $GIT_REMOTE_URL == https* ]]; then
@@ -31,9 +19,6 @@ fi
 
 git submodule init && git submodule update
 
-echo "Run $DOTFILES_ROOT/bashdot/bashdot install default $OS_NAME..."
-PROFILES="$OS_NAME default"
-
 # check if repo is still locked and should be unlocked
 if [ -s $DOTFILES_ROOT/git-crypt-status ]; then
   read -p "Dotfiles are still encrypted, do you want to decrypt? [yN]: " -n 1 -r; echo
@@ -43,11 +28,38 @@ if [ -s $DOTFILES_ROOT/git-crypt-status ]; then
       exit 1
     fi
     git-crypt unlock dotfiles-key
-    PROFILES="$PROFILES $OS_NAME-crypt default-crypt"
+    ENABLE_CRYPT=1
   fi
 else
-  PROFILES="$PROFILES $OS_NAME-crypt default-crypt"
+  ENABLE_CRYPT=1
 fi
+
+if [ "$(uname)" == "Darwin" ]; then
+  PROFILES="macos"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  if [ -f "/etc/arch-release" ]; then
+    PROFILES="arch linux"
+  fi
+  if [ $(awk -F= '/^NAME/{print $2}' /etc/os-release) == "\"Ubuntu\"" ]; then
+    PROFILES="ubuntu linux"
+
+    # check if stow is installed
+    if ! command -v stow &> /dev/null
+    then
+      sudo apt update && sudo apt install -y stow
+    fi
+  fi
+  if [ -n "$ENABLE_CRYPT" ]; then
+    PROFILES="$PROFILES linux-crypt"
+  fi
+fi
+
+PROFILES="$PROFILES default"
+if [ -n "$ENABLE_CRYPT" ]; then
+  PROFILES="$PROFILES default-crypt"
+fi
+
+echo "Run $DOTFILES_ROOT/bashdot/bashdot install $PROFILES..."
 
 # make sure that /usr/local/bin is in PATH
 export PATH="/usr/local/bin:$PATH"
