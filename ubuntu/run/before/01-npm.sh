3ed79c4c026960837f9bf4c043b6d6c44548dcdf
Jakob Koschel <jkl820.git@gmail.com>
2022-10-09 14:20:26 +0200

    ubuntu: update installing npm packages

--- ubuntu/run/before/01-npm.sh
+++ ubuntu/run/before/01-npm.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-if command -v npm &> /dev/null
-then
+NVM_DIR=$HOME/.nvm
+if [ -n $NVM_DIR ]; then
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
   npm -g install diff-so-fancy
 fi
