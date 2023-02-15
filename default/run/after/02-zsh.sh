cc45d8447efa97cdd80017532b73f1e4496999c3
Jakob Koschel <jkl820.git@gmail.com>
2023-02-15 11:15:07 +0100

    zsh: replace zplug with znap

--- default/run/after/02-zsh.sh
+++ default/run/after/02-zsh.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-if [ ! -e $HOME/.zplug ]; then
-  echo "Installing zplug..."
-  curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-else
-  zsh -ic "ZPLUG_PIPE_FIX=true zplug update"
+if command -v znap >/dev/null; then
+  znap pull
 fi
