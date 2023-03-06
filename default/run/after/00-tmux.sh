0877c8a99a904e8cfc9c7035e32796ee09dddbe9
Jakob Koschel <jkl820.git@gmail.com>
2023-03-06 20:40:01 +0100

    tmux: start and kill session to allow installing plugins

--- default/run/after/00-tmux.sh
+++ default/run/after/00-tmux.sh
@@ -8,5 +8,6 @@ if [ ! -e $HOME/.tmux/plugins/tpm ]; then
   git clone $TPM_GIT_REMOTE $HOME/.tmux/plugins/tpm
 fi
 
-tmux start-server
+tmux start-server && tmux new-session -d -t dotfiles-tmp
 tmux source $HOME/.tmux.conf && $HOME/.tmux/plugins/tpm/scripts/install_plugins.sh
+tmux kill-session -t dotfiles-tmp
