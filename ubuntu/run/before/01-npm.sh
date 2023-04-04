a1de4703b8e899bb2d8918cf850c6c2ca984548b
Jakob Koschel <jkl820.git@gmail.com>
2023-04-04 15:30:07 +0200

    ubuntu:npm: fix only using if installed

--- ubuntu/run/before/01-npm.sh
+++ ubuntu/run/before/01-npm.sh
@@ -3,7 +3,7 @@
 set -e
 
 NVM_DIR=$HOME/.nvm
-if [ -n $NVM_DIR ]; then
+if [ -a $NVM_DIR ]; then
   [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
   [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
   npm -g install diff-so-fancy
