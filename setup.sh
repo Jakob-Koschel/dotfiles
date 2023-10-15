ba4383c0dc02081ee117de6ba8d69ee184b55366
Jakob Koschel <jkl820.git@gmail.com>
2023-10-16 00:19:01 +0200

    setup.sh: improve linux dist detection

--- setup.sh
+++ setup.sh
@@ -54,8 +54,8 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
       sudo pacman -S --noconfirm stow
     fi
   fi
-  DISTRIBUTION="$(awk -F= '/^NAME/{print $2}' /etc/os-release)"
-  if [ "$DISTRIBUTION" == "\"Ubuntu\"" ] || [ "$DISTRIBUTION" == "\"Debian GNU/Linux\"" ]; then
+  DISTRIBUTION="$(awk -F= '/^NAME/{print $2}' /etc/os-release | tr -d '"')"
+  if [[ "$DISTRIBUTION" == "Ubuntu" ]] || [[ "$DISTRIBUTION" == "Debian GNU/Linux"* ]]; then
     PROFILES="ubuntu linux"
 
     # check if git-crypt is installed
