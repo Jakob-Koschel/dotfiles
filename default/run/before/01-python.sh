136416e1bfc16553c29264aac469b096d98a65a4
Jakob Koschel <jkl820.git@gmail.com>
2025-02-17 12:38:27 +0000

    python: fix virtualenvwrapper

--- default/run/before/01-python.sh
+++ default/run/before/01-python.sh
@@ -2,14 +2,15 @@
 
 set -e
 
+source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
+
 # virtualenv stuff is essential
 if ! command -v virtualenv &> /dev/null; then
   pip3 install --user virtualenv
 fi
-if ! command -v virtualenvwrapper.sh &> /dev/null; then
+if ! command -v virtualenvwrapper &> /dev/null; then
   pip3 install --user virtualenvwrapper
 fi
-
 
 # pynvim for neovim support
 if ! pip3 show pynvim >/dev/null; then
