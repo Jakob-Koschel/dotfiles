d53e17d85df5877476cc301236ce085bf4254801
Jakob Koschel <jkl820.git@gmail.com>
2026-06-29 22:40:07 +0200

    improve github-install.sh

--- github-install.sh
+++ github-install.sh
@@ -3,20 +3,20 @@
 # can be ran with:
 # bash <(curl -L -s https://raw.githubusercontent.com/Jakob-Koschel/dotfiles/main/github-install.sh)
 
-set -e
+set -euo pipefail
 
-git clone --recurse-submodules https://github.com/Jakob-Koschel/dotfiles.git
+if [ ! -d dotfiles ]; then
+  git clone --recurse-submodules https://github.com/Jakob-Koschel/dotfiles.git
+fi
 cd dotfiles
 
 # offer to replace the HTTPS clone URL with SSH for future pulls
-GIT_REMOTE_URL=$(git remote get-url origin)
-if [[ $GIT_REMOTE_URL == https* ]]; then
-  read -p "Repository is cloned via HTTPS. Should this be replaced with SSH? [yN]: " -n 1 -r; echo
+remote_url=$(git remote get-url origin)
+if [[ $remote_url == https://github.com/* ]]; then
+  read -rp "Repository is cloned via HTTPS. Should this be replaced with SSH? [yN]: " -n 1; echo
   if [[ $REPLY =~ ^[Yy]$ ]]; then
-    REPO=$(echo $GIT_REMOTE_URL | sed -n 's@^.*github.com/*/\([^)]*\).*$@\1@p')
-    USERNAME=${REPO%/*}
-    REPOSITORY=${REPO#*/}
-    git remote set-url origin "git@github.com:$USERNAME/$REPOSITORY"
+    # strip the prefix -> "OWNER/REPO.git", which is exactly the SSH path
+    git remote set-url origin "git@github.com:${remote_url#https://github.com/}"
   fi
 fi
 
